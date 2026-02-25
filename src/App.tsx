c1d5782fd0ba417bbbb7f517e9938f30995a72a2
martincarmna <mcarmonapalestina@gmail.com>
2026-02-25 15:04:05 -0600

    firt commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export function App(){
     return (
-        <h1>team </h1>
+          <h1 className="bg-green-500 h-25px relative text-white text-2xl font-bold text-center">
+    Hello world!
+  </h1>
     )
 }
